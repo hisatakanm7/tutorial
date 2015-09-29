7c3bf4dd31f37b599d0e426b58deebdabbf45cde
Yuki Oniki <yuki1508nm7@yahoo.co.jp>
2015-09-29 20:10:51 +0900

    complete 5

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -2,34 +2,45 @@ require "rails_helper"
 
 feature "Home page" do
   scenario "should have the content 'Sample App'" do
-    visit '/static_pages/home'
+    visit root_path
     expect(page).to have_content('Sample App')
   end
 
   scenario "should have the title 'Home'" do
-    visit '/static_pages/home'
+    visit root_path
     expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
   end
 end
 
 feature "Help page" do
   scenario "should have the content 'Help'" do
-    visit '/static_pages/help'
+    visit help_path
     expect(page).to have_content('Help')
   end
   scenario "should have the title 'Help'" do
-    visit '/static_pages/help'
+    visit help_path
     expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
   end
 end
 
 feature "About page" do
   scenario "should have the content 'About Us'" do
-    visit '/static_pages/about'
+    visit about_path
     expect(page).to have_content('About Us')
   end
   scenario "should have the title 'About Us'" do
-    visit '/static_pages/about'
+    visit about_path
     expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+  end
+end
+
+feature "Contact page" do
+  scenario "should have the content 'Contact'" do
+    visit contact_path
+    expect(page).to have_content('Contact')
+  end
+  scenario "should have the title 'Contact'" do
+    visit contact_path
+    expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
   end
 end
